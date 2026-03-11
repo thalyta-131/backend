adf1e087c1ced1bc3a7dbc7d16206681f2dc4e21
thalyta-131 <thalytaf171@gmail.com>
2026-03-11 10:47:53 -0300

    Update Shopee-Node.js

--- prompts/Shopee-Node.js
+++ prompts/Shopee-Node.js
@@ -1,1 +1,5 @@
 https://github.com/seu-usuario/carrinho-shopee-nodejs
+
+Projeto desenvolvido em Node.js que simula o funcionamento de um carrinho de compras da Shopee. O sistema permite adicionar produtos ao carrinho, remover itens,
+  calcular o valor total da compra e organizar os itens selecionados pelo usuário. O projeto utiliza conceitos de modularização, exportação de módulos (CommonJS/ESM), 
+  funções assíncronas e boas práticas de organização de código em Node.js.
