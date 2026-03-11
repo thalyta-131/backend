e5bd3a172603ecccda049d7507c2104e9d626066
thalyta-131 <thalytaf171@gmail.com>
2026-03-11 10:51:25 -0300

    Create Mario Kart-Node.js

---
+++ prompts/Mario Kart-Node.js
@@ -0,0 +1,3 @@
+https://github.com/seu-usuario/mario-kart-race-simulator-node
+Simulador de corridas inspirado no universo de Mario Kart, desenvolvido utilizando Node.js. O projeto simula uma corrida entre personagens clássicos da franquia,
+  utilizando lógica de programação para calcular velocidade, eventos aleatórios e determinar o vencedor da corrida.
