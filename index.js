7bcd192299e0313c78a4ff9df67e7d505e637bd8
thalyta-131 <thalytaf171@gmail.com>
2026-04-14 10:03:25 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+"name": "criando uma PI de blibioteca",
+"verson": "1.0.0",
+"description": Meu primeiro projeto",
+"main": "index.js",
+"scripts": {
+   "start": "node index.js",
+   "build": "webpack --mode production"
+},
+"dependencies": {
+  "react": "^18.2.0"
+},
+"devDependecies": {
+  "eslint": "^8.0.0"
+   }
+}
